f627b25dd805bc0f70191943f9ad049fb1719191
Graham McGregor <dev@grahammcgregor.ca>
2014-07-06 00:00:13 -0400

    Add output in case of long run time

--- validate_json.sh
+++ validate_json.sh
@@ -2,7 +2,12 @@
 
 function validate_json_files {
     files=( $(ls -1 | grep -i ".*\.json" | sed 's!\ !___!g') )
+    count=0
     for f in "${files[@]}"; do
+        (( count=count + 1 ))
+        if [[ $(expr $count % 500) -eq 0 ]]; then
+            echo "$count/${#files[@]}"
+        fi
         file_name=$( echo $f | sed 's!___!\\\ !g' )
         if [[ -f "$file_name" ]] && [[ ! -d "$file_name" ]] ; then
             cat $file_name | python -mjson.tool >/dev/null
